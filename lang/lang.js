d62b7e66791a3d9598d1be1acdcf4500fc28fe48
Juan Pablo <juampi92@gmail.com>
2014-08-07 21:16:39 -0300

    jsFormat Language

--- lang/lang.js
+++ lang/lang.js
@@ -1,32 +1,32 @@
 var fs = require('fs');
 
 var lang = {
-	deflang: 'es',
-	dicc: null,
-	setLang: function(lang){
-		this.dicc = null;
+  deflang: 'es',
+  dicc: null,
+  setLang: function(lang) {
+    this.dicc = null;
 
-		var lang_file = './lang/'+lang+'.json';
-		if ( ! fs.existsSync(lang_file) ) {
-			lang_file = './lang/'+this.deflang+'.json';
-			console.log(" That language does not exists! Using default ".red );
-		} 
-		
-		this.dicc = JSON.parse(fs.readFileSync(lang_file));
-		return this;
-	},
-	get: function(string){
-		// Eval propone problemas de responsabilidades. Usar con responsabilidad
-		return eval("this.dicc."+string);
-	},
-	trans: function(string, vars){
-		var trad = this.get(string);
-		if ( typeof vars === "string" ) vars = [vars];
-		
-		for (var i = 0; i < vars.length; i++)
-			trad = trad.replace("%"+(i+1), vars[i]);
-		return trad;
-	}
+    var lang_file = './lang/' + lang + '.json';
+    if (!fs.existsSync(lang_file)) {
+      lang_file = './lang/' + this.deflang + '.json';
+      console.log(" That language does not exists! Using default ".red);
+    }
+
+    this.dicc = JSON.parse(fs.readFileSync(lang_file));
+    return this;
+  },
+  get: function(string) {
+    // Eval propone problemas de responsabilidades. Usar con responsabilidad
+    return eval("this.dicc." + string);
+  },
+  trans: function(string, vars) {
+    var trad = this.get(string);
+    if (typeof vars === "string") vars = [vars];
+
+    for (var i = 0; i < vars.length; i++)
+      trad = trad.replace("%" + (i + 1), vars[i]);
+    return trad;
+  }
 };
 
 module.exports = lang;
